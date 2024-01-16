c820b39b4883a1a3b1693bdac334f342d48db5d2
timmySpark <timmyspark1@gmail.com>
2024-01-16 04:06:21 -0800

    [feat] :- updated place file, using Sqlalchemy

--- models/place.py
+++ models/place.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
 """place Module"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
 
+class Place(BaseModel, Base):
+    """Represents a Place for MySQL database
+    Inherits from BaseModel and Base (in this order)
+    Attributes:-
+        __tablename__(str): Represents Table name Places
+        city_id(sqlalchemy String): Represents city id, fkey to cities.id
+        user_id(sqlalchemy String): Represents user id, fkey to users.id
+        name(sqlalchemy String): Represents city name
+        description(sqlalchemy String): Represents city description
+        number_rooms(sqlalchemy Integer): Represents number of rooms
+        number_bathrooms(sqlalchemy Integer): Represents number of bathrooms
+        max_guest(sqlalchemy Integer): Represents number of maximum guests
+        price_by_night(sqlalchemy Integer): Represents price by night
+        Latitude(sqlalchemy Float): Represents Latitude
+        Longitude(sqlalchemy Float): Represents Longitude
+    fkey -- Foriegn key    
+    """
 
-class Place(BaseModel):
-    """This class Initializes place instances"""
-    city_id: str = ""
-    user_id: str = ""
-    name: str = ""
-    description: str = ""
-    number_rooms: int = 0
-    number_bathrooms: int = 0
-    max_guest: int = 0
-    price_by_night: int = 0
-    latitude: float = 0.0
-    longitude: float = 0.0
-    amenity_ids: list = []
+    __tablename__ = "places"
+    city_id = Column(String(60),ForeignKey('cities.id'), Nullable="False")
+    user_id = Column(String(60),ForeignKey('users.id'), Nullable="False")
+    name = Column(String(128), Nullable="False")
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
+    latitude = Column(Float) 
+    longitude = Column(Float)
