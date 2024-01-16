ef5c47530e076436c37f3a20c612eaa23ed02354
timmySpark <timmyspark1@gmail.com>
2024-01-16 04:05:57 -0800

    [feat] :- updated user file, using Sqlalchemy

--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
+class User(BaseModel, Base):
+    """ Represents a User in MySQL Database
+    Inherits from BaseModel and Base (in this order)
+    Attributes:
+        __tablename__(str) :- Represents table name users
+        email(sqlalchemy String) :- Represents User email Address
+        password(sqlalchmey String) :- Represents User password
+        first_name(sqlalchemy String) :- Represents User first name
+        last_name(sqlalchemy String) :- Represents User last name
+    """
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    email = Column(String(128), Nullable="False")
+    password = Column(String(128), Nullable="False")
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref= "user", cascade="delete")
