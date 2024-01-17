b1ccab7eeb42ec076e2bf2273ac3eaa9e9f23aaa
timmySpark <timmyspark1@gmail.com>
2024-01-16 22:55:23 -0800

    [feat] :- modified files, added relationships

--- models/place.py
+++ models/place.py
@@ -32,3 +32,5 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0)
     latitude = Column(Float) 
     longitude = Column(Float)
+    reviews = relationship("Review", backref="place", cascade="delete")
+
--- models/user.py
+++ models/user.py
@@ -22,4 +22,5 @@ class User(BaseModel, Base):
     password = Column(String(128), Nullable="False")
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref= "user", cascade="delete")
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
