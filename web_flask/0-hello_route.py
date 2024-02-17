0e42b0e5f5388b69db7d000a3d0a54a0e31bcf6a
timmySpark <timmyspark1@gmail.com>
2024-02-17 14:17:43 -0800

    [feat] :- created a script that starts a flask web app ad displays hbnb app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' A Module that contains a script that starts a Flask Web Application '''
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    ''' Displays Hello HBNB!. '''
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
